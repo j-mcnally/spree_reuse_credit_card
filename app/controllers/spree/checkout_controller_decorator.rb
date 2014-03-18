9cb52a4c704f2bb1968c4e3aa7436160ab8d0fc5
Andy Richardson <andy@kohactive.com>
2014-03-18 10:44:43 -0500

    added permitted params

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -4,36 +4,17 @@ module Spree
   CheckoutController.class_eval do
     include CardReuse
 
-    private
-
-    def before_payment
-      current_order.payments.destroy_all if request.put?
-      @cards = @order.user.credit_cards.available if @order.user.present?
+    before_filter do 
+      puts "permitted_checkout_attributes:: #{permitted_checkout_attributes.inspect}"
     end
 
-    # we are overriding this method in order to substitue in the exisiting card information
-    def object_params
-      # For payment step, filter order parameters to produce the expected nested attributes for a single payment and its source, discarding attributes for payment methods other than the one selected
+    private
 
-      if @order.has_checkout_step?("payment") && @order.payment?
-        if params[:payment_source].present?
-          source_params = params.delete(:payment_source)[params[:order][:payments_attributes].first[:payment_method_id].underscore]
+    Spree::PermittedAttributes.checkout_attributes << [:source, :source_attributes]
 
-          if params[:existing_card]
-            credit_card = Spree::CreditCard.find(params[:existing_card])
-            authorize! :manage, credit_card
-            params[:order][:payments_attributes].first[:source] = credit_card
-          else
-            if source_params
-              params[:order][:payments_attributes].first[:source_attributes] = source_params
-            end
-          end
-        end
-        if (params[:order][:payments_attributes])
-          params[:order][:payments_attributes].first[:amount] = @order.total
-        end
-      end
-      params[:order]
+    def before_payment
+      @order.payments.destroy_all if request.put? 
+      @cards = @order.user.credit_cards.available if @order.user.present?
     end
 
   end
